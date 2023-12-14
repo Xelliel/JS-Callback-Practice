1d17875cc68c30667e413d80c934e594341d7b81
Xelliel <malkomguile@yahoo.com>
2023-12-13 18:43:24 -0800

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const character = newImage('assets/green-character/static.gif');
+let x = 100;
+let y = 250;
+let direction = null;
+let isMoving = false;
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+function moveCharacter() {
+    character.style.left = x + 'px';
+    character.style.bottom = y + 'px';
+}
 
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowLeft':
+            x = Math.max(x - 5, 0);
+            direction = 'west';
+            isMoving = true;
+            break;
+        case 'ArrowUp':
+            y = Math.min(y + 5, window.innerHeight - character.height);
+            direction = 'north';
+            isMoving = true;
+            break;
+        case 'ArrowRight':
+            x = Math.min(x + 5, window.innerWidth - character.width);
+            direction = 'east';
+            isMoving = true;
+            break;
+        case 'ArrowDown':
+            y = Math.max(y - 5, 0);
+            direction = 'south';
+            isMoving = true;
+            break;
+        default:
+            break;
+    }
+});
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+document.addEventListener('keyup', () => {
+    direction = null;
+    isMoving = false;
+});
+
+setInterval(() => {
+    if (isMoving) {
+        if (direction === 'west') {
+            x = Math.max(x - 1, 0);
+        }
+        if (direction === 'north') {
+            y = Math.min(y + 1, window.innerHeight - character.height);
+        }
+        if (direction === 'east') {
+            x = Math.min(x + 1, window.innerWidth - character.width);
+        }
+        if (direction === 'south') {
+            y = Math.max(y - 1, 0);
+        }
+
+        moveCharacter();
+    }
+}, 100);
+
+move(newImage('assets/tree.png')).to(200, 450);
+move(newImage('assets/pillar.png')).to(350, 250);
+move(newImage('assets/pine-tree.png')).to(450, 350);
+move(newImage('assets/crate.png')).to(150, 350);
+move(newImage('assets/well.png')).to(500, 575);
+move(newItem('assets/sword.png')).to(500, 555);
+move(newItem('assets/shield.png')).to(165, 335);
+move(newItem('assets/staff.png')).to(600, 250);
